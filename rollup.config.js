f23943907765be58e06001fdffe3865315e5f2af
huangjs888 <1170384651@qq.com>
2023-08-21 15:16:22 +0800

    构建修改

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@
  * @Author: Huangjs
  * @Date: 2023-08-09 11:24:45
  * @LastEditors: Huangjs
- * @LastEditTime: 2023-08-16 17:12:02
+ * @LastEditTime: 2023-08-21 14:29:54
  * @Description: ******
  */
 
@@ -47,7 +47,7 @@ const config = {
     babel({
       babelHelpers: MOD_ENV === 'esm' ? 'bundled' : 'runtime',
       include: 'src/**/*',
-      exclude: MOD_ENV === 'esm' ? '**/node_modules/**' : undefined,
+      exclude: '**/node_modules/**',
       extensions,
     }),
   ],
